28ee0b0d172e3ee818379814043b0ffa9827646e
Arthur Mancio Sales <arthur_mancio@outlook.com>
2025-07-23 18:33:07 -0300

    build: update route to build for production

--- frontend/src/routes/index.ts
+++ frontend/src/routes/index.ts
@@ -12,7 +12,6 @@ export const routes = {
   ...paymentRoutes,
   verifyExistingDocRequest: routeBuilder(
     ["POST"],
-    () =>
-      "http://127.0.0.1:5001/van-veen-workshop/us-central1/verifyExistingDocRequest",
+    () => "https://verifyexistingdocrequest-566pzglqfa-uc.a.run.app",
   ),
 };
--- frontend/src/routes/payment.ts
+++ frontend/src/routes/payment.ts
@@ -3,7 +3,6 @@ import { routeBuilder } from "@/utils/routeBuilder";
 export const paymentRoutes = {
   payment: routeBuilder(
     ["POST"],
-    () =>
-      "https://us-central1-van-veen-workshop.cloudfunctions.net/process_payment",
+    () => "https://process-payment-566pzglqfa-uc.a.run.app",
   ),
 };
